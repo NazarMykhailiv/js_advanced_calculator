9d2bc20843ef585bbe0f35da4624785cb74f8bee
NazarMykhailv <mihailiv1011@gmail.com>
2025-03-17 13:42:45 +0200

    add solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -5,6 +5,41 @@
  */
 function makeCalculator() {
   // write code here
+  return {
+    result: 0,
+
+    add(num) {
+      return this.result + num;
+    },
+
+    subtract(num) {
+      return this.result - num;
+    },
+
+    multiply(num) {
+      return this.result * num;
+    },
+
+    divide(num) {
+      if (num === 0) {
+        throw new Error('Cannot divide by zero!');
+      }
+
+      return this.result / num;
+    },
+
+    operate(operation, num) {
+      this.result = operation.call(this, num);
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
 }
 
 module.exports = makeCalculator;
